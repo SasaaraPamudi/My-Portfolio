6cd9f660f15056e4fa6253458cf8a2381e41ba7c
sasaara <pamudisasaara@gmail.com>
2026-06-21 17:53:34 +0530

    Initial commit

---
+++ cinematic-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './Hero';
+
+function App() {
+  return (
+      <main className="w-full min-h-screen bg-[#0d0d0d]">
+        <Hero />
+      </main>
+  );
+}
+
+export default App;
